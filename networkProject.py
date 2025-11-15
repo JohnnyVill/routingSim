08713abeb4730e929ee5ad565d4e21298f536a24
Johnny <j_villanueva11@u.pacific.edu>
2025-11-15 14:51:01 -0800

    throughput added

--- networkProject.py
+++ networkProject.py
@@ -1,3 +1,4 @@
+import math
 class Router:
     def __init__ (self,name):
         self.name = name
@@ -18,17 +19,25 @@ class Network:
             self.routers[name] = Router(name)
         else:
             return f"Router {name} already exists"
+        
+    def total_nodal_delay(self, dproc, dqueue, transRate,dprop,packet_size):
+        dtrans = (packet_size/ (transRate * 1000000)) * 1000
+        dnodal = dproc + dqueue + dtrans + dprop
+        throughput = round((packet_size/1000000) / (dnodal/1000),2)
+        return throughput
     
-    def connect(self, name1, name2, cost= 1):
+    
+    def connect(self, name1, name2, dproc, dqueue, trans, dprop = 0 ,packet_size = 50000):
         if name1 in self.routers and name2 in self.routers:
-            self.routers[name1].add_connections(name2, cost)
-            self.routers[name2].add_connections(name1, cost)
+            self.routers[name1].add_connections(name2, {"throughput_Mbps": self.total_nodal_delay(dproc, dqueue, trans, dprop, packet_size)})
+            self.routers[name2].add_connections(name1, {"throughput_Mbps":self.total_nodal_delay(dproc, dqueue, trans, dprop, packet_size)})
         else:
             if name1 in self.routers:
                 return f"Router {name2} does not exist"
             else:
                 return f"Router {name1} does not exist"
     
+   
     def show_network(self):
         for router in self.routers.values():
             print(router)
@@ -39,10 +48,15 @@ def main():
     net.add_router("R1")
     net.add_router("R2")
     net.add_router("R3")
-
-    net.connect("R1", "R2", 2)
-    net.connect("R2", "R3", 4)
-    net.connect("R1", "R3", 7)
+    #   to connect a node add the nodes name the node it want to connect to 
+    #   and the dproc, dqueue, trans, packet size (dprop, packet size is optional)
+    #ex) net.connect(node1, node1, 3, 4, 10, 10000000)
+    # we are assuming all transmission rate are in megabits
+    # dprop is 0ms by default
+    # packet size is 10,000 by default and is in bits
+    net.connect("R1", "R2", 3, 10, 5, 1, 50000)
+    net.connect("R2", "R3", 2,1,10,0.5,80000)
+    net.connect("R1", "R3", 7,7,7, 0, 60000)
 
     net.show_network()
 
